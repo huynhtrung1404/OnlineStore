a97b5239ae5b6ab0de8b1aeaeb9cfffe746f5886
huynhtrung1404 <huynh.trung140495@outlook.com>
2024-05-22 21:50:47 +0700

    Updated code

--- Backend/OnlineStore.Application/GlobalUsing.cs
+++ Backend/OnlineStore.Application/GlobalUsing.cs
@@ -3,4 +3,3 @@ global using OnlineStore.Application.DTOs;
 global using OnlineStore.Application.Commons.Interfaces;
 global using OnlineStore.Shared.Common.Constants;
 global using OnlineStore.Application.Responses;
-global using OnlineStore.Application.Tokens;
