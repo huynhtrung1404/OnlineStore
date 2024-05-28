3b865802921557e59b486489f46fa80991b191ac
huynhtrung1404 <huynh.trung140495@outlook.com.vn>
2024-05-28 16:48:45 +0700

    updated code for react

--- Frontend/OnlineStore.Web/src/Pages/Accounts/Register/index.tsx
+++ Frontend/OnlineStore.Web/src/Pages/Accounts/Register/index.tsx
@@ -1,39 +1,72 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
-import { FloatLabel } from "primereact/floatlabel";
+import { InputTextarea } from "primereact/inputtextarea";
 
 function Register() {
   const [firstName, setFirstName] = useState<string>("");
   const [lastName, setLastName] = useState<string>("");
-  const [userName, setUserName] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [email, setEmail] = useState<string>("email");
-  const [dob, setDob] = useState<string>("");
-  const [address, setAddress] = useState<string>("");
-  const [address2, setAddress2] = useState<string>("");
-  const [city, setCity] = useState<string>("");
-  const [provine, setProvice] = useState<string>("");
-  const [gender, setGender] = useState<number>(0);
+  // const [userName, setUserName] = useState<string>("");
+  // const [password, setPassword] = useState<string>("");
+  // const [email, setEmail] = useState<string>("email");
+  // const [dob, setDob] = useState<string>("");
+  // const [address, setAddress] = useState<string>("");
+  // const [address2, setAddress2] = useState<string>("");
+  // const [city, setCity] = useState<string>("");
+  // const [provine, setProvice] = useState<string>("");
+  // const [gender, setGender] = useState<number>(0);
   return (
-    <div className="justify-content-center">
-      <h1>Register new account</h1>
-      <div className="card flex justify-content-center">
-        <FloatLabel>
+    <div className="flex justify-content-center" style={{ maxWidth: "500px" }}>
+      <h1 className="flex align-item-center justify-content-center">
+        Register an account
+      </h1>
+      <div className="formgrid grid flex align-items-center justify-content-center">
+        <div className="field col-12 md:col-6">
+          <label htmlFor="firstname6">Firstname</label>
           <InputText
-            id="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
+            id="firstname6"
+            type="text"
+            className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
           />
-          <label htmlFor="firstName">First Name</label>
-        </FloatLabel>
-        <FloatLabel>
+        </div>
+        <div className="field col-12 md:col-6">
+          <label htmlFor="lastname6">Lastname</label>
           <InputText
-            id="lastName"
-            value={firstName}
-            onChange={(e) => setLastName(e.target.value)}
+            id="lastname6"
+            type="text"
+            className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
           />
-          <label htmlFor="lastName">Last Name</label>
-        </FloatLabel>
+        </div>
+        <div className="field col-12">
+          <label htmlFor="address">Address</label>
+          <InputTextarea
+            id="address"
+            className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
+          />
+        </div>
+        <div className="field col-12 md:col-6">
+          <label htmlFor="city">City</label>
+          <InputText
+            id="city"
+            type="text"
+            className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
+          />
+        </div>
+        <div className="field col-12 md:col-3">
+          <label htmlFor="state">State</label>
+          <InputText
+            id="state"
+            type="text"
+            className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
+          />
+        </div>
+        <div className="field col-12 md:col-3">
+          <label htmlFor="zip">Zip</label>
+          <InputText
+            id="zip"
+            type="text"
+            className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full"
+          />
+        </div>
       </div>
     </div>
   );
--- Frontend/OnlineStore.Web/src/main.tsx
+++ Frontend/OnlineStore.Web/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import "primeflex/primeflex.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
